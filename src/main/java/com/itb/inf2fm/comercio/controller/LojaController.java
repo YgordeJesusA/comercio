7f85c14f42d51025e8cb54a1035c322a722b3474
Ygor de Jesus Araújo <ygorjesus886@gmail.com>
2023-06-14 08:09:42 -0300

    inicio controle de versao

---
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -0,0 +1,26 @@
+package com.itb.inf2fm.comercio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itb.inf2fm.comercio.model.Produto;
+
+@Controller
+@RequestMapping("/comercio/produtos")
+public class LojaController {
+
+	
+	
+	@GetMapping("/listar")
+	public String listarProdutos() {
+		
+		
+		Produto p1 = new Produto();
+		p1.id = 20l;
+		
+		
+		return "produtos";
+		
+	}
+}
---
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -0,0 +1,51 @@
+package com.itb.inf2fm.comercio.model;
+
+public class Produto {
+
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String codigoBarras;
+	private double preco;
+	
+	// public : Acesso liberado para todas as classes.
+	
+	// private: Acesso permitido apenas para os membros
+	//          da própria classe. 
+	//          Entende-se por membros(atributos e ou métodos)
+	
+	// Métodos Setter´s e Getter's : atribuir e recuperar dados do atributo "RESPECTIVAMENTE"
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+}
