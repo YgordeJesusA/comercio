19dc80ae6dc0aeed6dc3069b97ddcecb0cae6562
Ygor de Jesus Araújo <ygorjesus886@gmail.com>
2023-06-19 08:04:33 -0300

    Editando o template produtos

--- src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -1,16 +1,12 @@
 package com.itb.inf2fm.comercio.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.itb.inf2fm.comercio.model.Produto;
+import java.util.Arraylist;
 
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
-
-	
 	
 	@GetMapping("/listar")
 	public String listarProdutos() {
